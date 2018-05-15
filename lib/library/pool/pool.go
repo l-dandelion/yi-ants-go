bbc20c95d423bf0cf80fec9c75c6e60fd37c65c0
l-dandelion <18826136742@163.com>
2018-05-16 00:55:43 +0800

    Update pool.go

--- lib/library/pool/pool.go
+++ lib/library/pool/pool.go
@@ -6,42 +6,46 @@ import (
 
 type Pool struct {
 	pool  chan bool      //控制数量
-	group sync.WaitGroup //用于等待所有爬虫执行完毕
+	group sync.WaitGroup //用于等待所有goroutine执行完毕
 }
 
-//增加爬虫
+//添加等待goroutine的数量
 func (pool *Pool) Add() {
 	pool.pool <- true
 	pool.group.Add(1)
 }
 
-//爬虫结束
+//减少等待groutine的数量
 func (pool *Pool) Done() {
 	<-pool.pool
 	pool.group.Done()
 }
 
-//等待所有爬虫结束
+//等待groutine的数量为0
 func (pool *Pool) Wait() {
 	pool.group.Wait()
 }
 
+//初始化等待goroutine的最大数量
 func (pool *Pool) Init(maxNum int) {
 	pool.pool = make(chan bool, maxNum)
 }
 
-//进程池中已分配的数量
-func (pool *Pool) Num() int {
-	return len(pool.pool)
-}
-
-//进程池容量
-func (pool *Pool) Cap() int {
-	return cap(pool.pool)
-}
-
+//新建并发控制器
 func NewPool(maxNum int) *Pool {
 	return &Pool{
 		pool: make(chan bool, maxNum),
 	}
 }
+
+
+//goroutine池中已分配的数量
+func (pool *Pool) Num() int {
+	return len(pool.pool)
+}
+
+//goroutine池容量
+func (pool *Pool) Cap() int {
+	return cap(pool.pool)
+}
+
